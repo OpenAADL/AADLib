7238b4e3b876a1a260ec1b5ed0f0c0e7bb9fff5b
yoogx <hugues.jerome@gmail.com>
2013-02-28 14:38:16 +0100

    * Update model to support code generation towards Ada

--- examples/producer_consumer/producer_consumer.ads
+++ examples/producer_consumer/producer_consumer.ads
@@ -1,37 +1,3 @@
-------------------------------------------------------------------------------
---                                                                          --
---                          PolyORB HI COMPONENTS                           --
---                                                                          --
---                    P R O D U C E R _ C O N S U M E R                     --
---                                                                          --
---                                 S p e c                                  --
---                                                                          --
---                 Copyright (C) 2009, GET-Telecom Paris.                   --
---                                                                          --
--- PolyORB HI is free software; you  can  redistribute  it and/or modify it --
--- under terms of the GNU General Public License as published by the Free   --
--- Software Foundation; either version 2, or (at your option) any later.    --
--- PolyORB HI is distributed  in the hope that it will be useful, but       --
--- WITHOUT ANY WARRANTY;  without even the implied warranty of              --
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General --
--- Public License for more details. You should have received  a copy of the --
--- GNU General Public  License  distributed with PolyORB HI; see file       --
--- COPYING. If not, write  to the Free  Software Foundation, 51 Franklin    --
--- Street, Fifth Floor, Boston, MA 02111-1301, USA.                         --
---                                                                          --
--- As a special exception,  if other files  instantiate  generics from this --
--- unit, or you link  this unit with other files  to produce an executable, --
--- this  unit  does not  by itself cause  the resulting  executable  to  be --
--- covered  by the  GNU  General  Public  License.  This exception does not --
--- however invalidate  any other reasons why  the executable file  might be --
--- covered by the  GNU Public License.                                      --
---                                                                          --
---                PolyORB HI is maintained by GET Telecom Paris             --
---                                                                          --
-------------------------------------------------------------------------------
-
---  $Id: producer_consumer.ads 6274 2009-03-25 19:56:54Z lasnier $
-
 with PolyORB_HI_Generated.Types; use PolyORB_HI_Generated.Types;
 
 package Producer_Consumer is
