e6de8ab7fa87d9cc16dfc80fe22991038a6d2877
Stachelbeck <kenneth.stachelbeck@gtri.gatech.edu>
2013-03-26 17:44:20 -0700

    Update M1RTMgr.c
    Change print messages

--- examples/multicore/M1RTMgr.c
+++ examples/multicore/M1RTMgr.c
@@ -6,22 +6,22 @@ static uint32_t	MessageCounter		= 0;
 
 
 void m1rtinit(initmsg *msg){
-	printf("Mission Bus 1 Remote Terminal Initialization - Start\n");
+	printf("RT Bus Manager 1 Remote Terminal Initialization - Start\n");
 	msg->InitializationComplete = TRUE;
-	printf("Mission Bus 1 Remote Terminal  - Complete\n");
+	printf("RT Bus Manager 1 Remote Terminal  - Complete\n");
 }
 
 void m1rtmgr(initmsg msg,busmsg *RTtoBCMsg){
 	if(msg.InitializationComplete){
 		if(FirstTimeThrough){
-			printf("Mission Bus 1 Remote Terminal  - Start\n");
+			printf("RT Bus Manager 1 Remote Terminal  - Start\n");
 			FirstTimeThrough = FALSE;
 		}
 		if(MessageCounter == 0){
 			RTtoBCMsg->MsgQueue[0].Msg[0]=1;
 			RTtoBCMsg->MsgQueue[0].Msg[1]++;
 			MessageCounter = 800;				// 8 Seconds
-			printf("RT1 Sending Message (%d) to BC\n",RTtoBCMsg->MsgQueue[0].Msg[1]);
+			printf("RT Bus Mgr 1 Sending Message (%d) to BC\n",RTtoBCMsg->MsgQueue[0].Msg[1]);
 		}
 		else {
 			RTtoBCMsg->MsgQueue[0].Msg[0]=0;
