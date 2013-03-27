20b2810c37a86aaac84be300f61ac30d38375921
Stachelbeck <kenneth.stachelbeck@gtri.gatech.edu>
2013-03-26 17:45:14 -0700

    Update M2RTMgr.c
    Change print messages

--- examples/multicore/M2RTMgr.c
+++ examples/multicore/M2RTMgr.c
@@ -7,22 +7,22 @@ static uint32_t	MessageCounter	= 0;
 
 
 void m2rtinit(initmsg *msg){
-	printf("Mission Bus 2 Remote Terminal Initialization - Start\n");
+	printf("RT Bus Manager 2 Remote Terminal Initialization - Start\n");
 	msg->InitializationComplete = TRUE;
-	printf("Mission Bus 2 Remote Terminal  - Complete\n");
+	printf("RT Bus Manager 2 Remote Terminal  - Complete\n");
 }
 
 void m2rtmgr(initmsg msg, busmsg *RTtoBCMsg){
 	if(msg.InitializationComplete){
 		if(FirstTimeThrough){
-			printf("Mission Bus 2 Remote Terminal  - Start\n");
+			printf("RT Bus Manager 2 Remote Terminal  - Start\n");
 			FirstTimeThrough = FALSE;
 		}
 		if(MessageCounter == 0){
 			RTtoBCMsg->MsgQueue[0].Msg[0]=1;
 			RTtoBCMsg->MsgQueue[0].Msg[1]++;
 			MessageCounter = 500;				// 5 seconds
-			printf("RT2 Sending Message (%d) to BC\n",RTtoBCMsg->MsgQueue[0].Msg[1]);
+			printf("RT Bus Mgr 2 Sending Message (%d) to BC\n",RTtoBCMsg->MsgQueue[0].Msg[1]);
 		}
 		else {
 			RTtoBCMsg->MsgQueue[0].Msg[0]=0;
