7eb83b48b572368af941a35695908f5f19d5bdbd
yoogx <hugues.jerome@gmail.com>
2019-05-15 21:06:17 -0400

    * Added consume_spg()

--- examples/producer_consumer_ba/producer_consumer.c
+++ examples/producer_consumer_ba/producer_consumer.c
@@ -5,4 +5,11 @@ void print_spg
 (producer__consumer__alpha_type a_data_in) {
 
   printf("%d\n", a_data_in);
+  fflush(stdout);
+}
+
+void consume_spg (producer__consumer__alpha_type data)
+{
+   printf ("Consume %d\n", data);
+   fflush(stdout);
 }
