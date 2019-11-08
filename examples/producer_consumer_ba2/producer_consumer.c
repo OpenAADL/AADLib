1022627b4ee06fdc72181d3e7052915aad544b89
bouazizrahma <bouaziz.rahma@gmail.com>
2019-11-08 10:57:33 +0100

    example to test different data structures (array, struct, enum) and control structures (if, for, forall et while)

---
+++ examples/producer_consumer_ba2/producer_consumer.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<types.h>
+
+void print_spg
+//(producer__consumer__alpha_type a_data_in) {
+(test__ba__backend__a_struct_impl a_data_in) {
+
+  printf("****************************************** \n");
+  printf("#elems of a_data = %d\n", a_data_in.elem);
+  fflush(stdout);
+  for (int i = 0; i < a_data_in.elem; i++)
+  { 
+
+  	printf("a_data.all_elems [%d] = %d\n", i, a_data_in.all_elems [i]);
+  	fflush(stdout);
+  }
+  printf("****************************************** \n");
+}
+
+//void consume_spg (producer__consumer__alpha_type data)
+void consume_spg (test__ba__backend__a_struct_impl data)
+{
+   printf ("Consume %d\n", data.elem);
+   fflush(stdout);
+}
