a5e434e514fd4993f82441d0141c8a4ae6967a40
Jerome Hugues <hugues.jerome@gmail.com>
2020-05-04 03:30:44 +0100

    * Minor bugfixes

--- examples/producer_consumer_ba_many_transitions/producer_consumer.c
+++ examples/producer_consumer_ba_many_transitions/producer_consumer.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<types.h>
 #include<stdlib.h>
-#include <time.h> 
+#include <time.h>
 
 int nb_call_of_compute_spg = 1;
 
@@ -9,28 +9,28 @@ int nb_call_of_compute_spg = 1;
 void compute_spg (int* a_data_out) {
 
     if (nb_call_of_compute_spg == 1)
-    	// Use current time as seed for random generator 
-    	srand(time(0)); 
+        // Use current time as seed for random generator
+        srand(time(0));
 
-		nb_call_of_compute_spg++;
+                nb_call_of_compute_spg++;
 
     /* random int between 0 and 20 */
-    int r = rand() % 20 + 1; 
-    printf ("compute_spg : value = %d \n", r);   
-    *a_data_out = r;        
+    int r = rand() % 20 + 1;
+    printf ("compute_spg : value = %d \n", r);
+    *a_data_out = r;
 }
 
 void print_spg
-(test__ba__backend__a_struct_impl a_data_in) {
+(test__ba__backend_ba_v3__a_struct_impl a_data_in) {
 
   printf("****************************************** \n");
   printf("#elems of consumed data = %d\n", a_data_in.elem);
   fflush(stdout);
   for (int i = 0; i < a_data_in.elem; i++)
-  { 
+  {
 
-  	printf("Consumed data.all_elems [%d] = %d\n", i, a_data_in.all_elems [i]);
-  	fflush(stdout);
+        printf("Consumed data.all_elems [%d] = %d\n", i, a_data_in.all_elems [i]);
+        fflush(stdout);
   }
   printf("****************************************** \n");
 }
@@ -43,4 +43,3 @@ void print_thread_begin_execution ()
   printf("*--------------------------------------------* \n");
   fflush(stdout);
 }
-
